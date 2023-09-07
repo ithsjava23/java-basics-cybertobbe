1eea40f9a32a64ae9f8086190de3b0124bb03237
cybertobbe <tobias.sjostrom@iths.se>
2023-09-07 11:09:20 +0200

    Menu3. sortingPrices works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,40 +10,38 @@ public class App {
         String menuChoise = "";
 
 
-        String [] timeArray = new String[]{
+        String[] timeArray = new String[]{
                 "00-01", "01-02", "02-03", "03-04", "04-05", "05-06",
                 "06-07", "07-08", "08-09", "09-10", "10-11", "11-12",
                 "12-13", "13-14", "14-15", "15-16", "16-17", "17-18",
                 "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"
         };
 
-        String [] timeArray2 = new String[]{
+        String[] timeArray2 = new String[]{
                 "00-01", "01-02", "02-03", "03-04", "04-05", "05-06",
                 "06-07", "07-08", "08-09", "09-10", "10-11", "11-12",
                 "12-13", "13-14", "14-15", "15-16", "16-17", "17-18",
                 "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"
         };
-        int [] energyPrices = new int[24];
-        int [] energyPrices2 = new int[24];
+        int[] energyPrices = new int[24];
+        int[] energyPrices2 = new int[24];
 
-        do{
+        do {
             showMenu();
             menuChoise = scanner.nextLine();
 
-            switch(menuChoise){
+            switch (menuChoise) {
                 case "1":
                     addEnergyPrices(scanner, energyPrices);
-
-
-                    for(int i = 0; i<energyPrices.length; i++){
+                    for (int i = 0; i < energyPrices.length; i++) {
                         System.out.println(timeArray[i] + "\t" + energyPrices[i] + " öre/kWh" + "\n");
                     }
                     break;
                 case "2":
-
                     findMaxMin(energyPrices, timeArray);
                     break;
                 case "3":
+                    sortingPrices(energyPrices, timeArray);
                     break;
                 case "4":
                     break;
@@ -51,11 +49,11 @@ public class App {
 
             }
 
-        }while(!"e".equals(menuChoise) && !"E".equals(menuChoise));
+        } while (!"e".equals(menuChoise) && !"E".equals(menuChoise));
 
     }
 
-    public static void showMenu(){
+    public static void showMenu() {
 
         String appMenu = """
                 Elpriser
@@ -70,9 +68,9 @@ public class App {
 
     }
 
-    public static void addEnergyPrices(Scanner scanner, int energyPrices []){
+    public static void addEnergyPrices(Scanner scanner, int energyPrices[]) {
 
-        for(int i = 0; i<energyPrices.length; i++) {
+        for (int i = 0; i < energyPrices.length; i++) {
             System.out.print("Ange elpris: \n");
             int price = scanner.nextInt();
             energyPrices[i] = price;
@@ -82,33 +80,31 @@ public class App {
 
     }
 
-    public static void findMaxMin(int arrayPrice[], String arrayTime[]){
+    public static void findMaxMin(int arrayPrice[], String arrayTime[]) {
 
         //Find min price and calculate mean price
         double mean = 0;
         int min = arrayPrice[0];
         int indexMin = 0;
-        for(int i = 0; i<arrayPrice.length; i++){
+        for (int i = 0; i < arrayPrice.length; i++) {
 
             mean = mean + arrayPrice[i];
 
-            if(arrayPrice[i] < min){
+            if (arrayPrice[i] < min) {
                 min = arrayPrice[i];
                 indexMin = i;
 
 
-
-
             }
         }
-        mean = (mean/24);
+        mean = (mean / 24);
 
         //Find max price
         int max = arrayPrice[0];
         int indexMax = 0;
-        for(int i = 0; i<arrayPrice.length; i++){
+        for (int i = 0; i < arrayPrice.length; i++) {
 
-            if(arrayPrice[i] > max){
+            if (arrayPrice[i] > max) {
                 max = arrayPrice[i];
                 indexMax = i;
             }
@@ -132,6 +128,27 @@ public class App {
         System.out.println(MinMaxMean);
     }
 
+    public static void sortingPrices(int[] arrPrice, String[] timeArray){
+        int tempPrice;
+        String tempString;
 
+        for (int i = 0; i < arrPrice.length-1; i++) {
+            for (int j = 0; j < arrPrice.length - i -1; j++) {
+                {
+                    if(arrPrice[j] < arrPrice[j+1]){
+                        tempPrice = arrPrice[j];
+                        arrPrice[j] = arrPrice[j+1];
+                        arrPrice[j+1] = tempPrice;
 
+                        tempString = timeArray[j];
+                        timeArray[j] = timeArray[j+1];
+                        timeArray[j+1] = tempString;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < arrPrice.length; i++) {
+            System.out.print(timeArray[i] + " " + arrPrice[i] + " öre\n");
+        }
+    }
 }
