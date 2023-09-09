d749fbdfa6f8e47d4c8551931bb1782845cc27aa
cybertobbe <tobias.sjostrom@iths.se>
2023-09-09 09:31:34 +0200

    Allt grönt

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,30 +1,30 @@
 package org.example;
 
-import java.text.DecimalFormat;
+import java.util.Arrays;
+
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+
+
+
         Scanner scanner = new Scanner(System.in);
         String menuChoise = "";
 
 
         String[] timeArray = new String[]{
-                "00-01", "01-02", "02-03", "03-04", "04-05", "05-06",
-                "06-07", "07-08", "08-09", "09-10", "10-11", "11-12",
-                "12-13", "13-14", "14-15", "15-16", "16-17", "17-18",
-                "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"
+                "00-01", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12",
+                "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"
         };
 
-        String[] timeArray2 = new String[]{
-                "00-01", "01-02", "02-03", "03-04", "04-05", "05-06",
-                "06-07", "07-08", "08-09", "09-10", "10-11", "11-12",
-                "12-13", "13-14", "14-15", "15-16", "16-17", "17-18",
-                "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"
-        };
+
         int[] energyPrices = new int[24];
-        int[] energyPrices2 = new int[24];
+        int[] energyPricesCopied = new int[24];
+
 
         do {
             showMenu();
@@ -32,7 +32,7 @@ public class App {
 
             switch (menuChoise) {
                 case "1":
-                    addEnergyPrices(scanner, energyPrices);
+                    addEnergyPrices(scanner, energyPrices, energyPricesCopied);
                     for (int i = 0; i < energyPrices.length; i++) {
                         System.out.println(timeArray[i] + "\t" + energyPrices[i] + " öre/kWh" + "\n");
                     }
@@ -44,6 +44,7 @@ public class App {
                     sortingPrices(energyPrices, timeArray);
                     break;
                 case "4":
+                    cheapestFourHours(energyPricesCopied, timeArray);
                     break;
 
 
@@ -68,7 +69,7 @@ public class App {
 
     }
 
-    public static void addEnergyPrices(Scanner scanner, int energyPrices[]) {
+    public static void addEnergyPrices(Scanner scanner, int energyPrices[], int energyPricesCopied[]) {
 
         for (int i = 0; i < energyPrices.length; i++) {
             System.out.print("Ange elpris: \n");
@@ -76,11 +77,12 @@ public class App {
             energyPrices[i] = price;
 
         }
-
+        //Copy a unsorted array of energyPrices.
+        System.arraycopy(energyPrices,0,energyPricesCopied,0,energyPrices.length);
 
     }
 
-    public static void findMaxMin(int arrayPrice[], String arrayTime[]) {
+    public static void findMaxMin(int[] arrayPrice, String[] arrayTime) {
 
         //Find min price and calculate mean price
         double mean = 0;
@@ -150,5 +152,41 @@ public class App {
         for (int i = 0; i < arrPrice.length; i++) {
             System.out.print(timeArray[i] + " " + arrPrice[i] + " öre\n");
         }
+    }
+
+    public static void cheapestFourHours(int[] energyPricesCopied, String[] timeArray){
+        /*double energyPriceOverFourHours =  Double.MAX_VALUE;
+        String startHour = "";
+        double fourHourMeanPrice;
+
+        for(int i = 0; i<energyPricesCopied.length-4; i++){
+            if(energyPriceOverFourHours > (energyPricesCopied[i] + energyPricesCopied[i+1] + energyPricesCopied[i+2] + energyPricesCopied[i+3])){
+                energyPriceOverFourHours = (energyPricesCopied[i] + energyPricesCopied[i+1] + energyPricesCopied[i+2] + energyPricesCopied[i+3]);
+
+            }
+
+            startHour = timeArray[i].substring(0,2);
+
+        }
+
+        fourHourMeanPrice = energyPriceOverFourHours/4;
+
+        String response = """
+                Påbörja Laddning klockan %s
+                Medelpris 4h: %.1f öre/KWh
+                """;
+        String cheapestTime = String.format(response, startHour, fourHourMeanPrice);
+        System.out.print("\n" + cheapestTime);*/
+
+        float fourHourMeanPrice = energyPricesCopied[0] + energyPricesCopied[1] + energyPricesCopied[2] + energyPricesCopied[3];
+        String startPoint = timeArray[0];
+        for (int i = 1; i < timeArray.length - 3; i++) {
+            if (fourHourMeanPrice > (energyPricesCopied[i] + energyPricesCopied[i + 1] + energyPricesCopied[i + 2] + energyPricesCopied[i + 3])) {
+                fourHourMeanPrice = (energyPricesCopied[i] + energyPricesCopied[i + 1] + energyPricesCopied[i + 2] + energyPricesCopied[i + 3]);
+                startPoint = timeArray[i];
+            }
+        }
+        String[] print = startPoint.split("-");
+        System.out.printf(Locale.getDefault(),"Påbörja laddning klockan %s\nMedelpris 4h: %.1f öre/kWh\n",print[0], (fourHourMeanPrice / 4));
     }
 }
